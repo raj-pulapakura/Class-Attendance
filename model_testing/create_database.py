31c6ee300c87c004ccf6211f28b2496aa0b22496
Raj Pulapakura <raj.pulapakura@gmail.com>
2023-06-27 23:28:55 +1000

    added description to script create_database

--- model_testing/create_database.py
+++ model_testing/create_database.py
@@ -1,3 +1,20 @@
+""" Image Face Database Creator
+
+usage: create_database [-h] [-i IMG_FOLDER] [-l LABEL_MAP_CSV] [-o OUTPUT_PATH]
+
+Extracts the face from an image and saves it to a database of images, sorted by the identity of the face
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -i IMG_FOLDER, --img_folder IMG_FOLDER
+                        Path to the folder of images
+  -l LABEL_MAP_CSV, --label_map_csv LABEL_MAP_CSV
+                        Path to the CSV of labels (file:label) pairs
+  -o OUTPUT_PATH, --output_path OUTPUT_PATH
+                        Path to folder which is the database
+"""
+
+
 import argparse
 import os
 import cv2
@@ -16,12 +33,12 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument("-i", "--img_folder", type=str, help="Path to the folder of images")
 parser.add_argument("-l", "--label_map_csv", type=str, help="Path to the CSV of labels (file:label) pairs")
-parser.add_argument("-o", "--output", type=str, help="Path to folder which is the database")
+parser.add_argument("-o", "--output_path", type=str, help="Path to folder which is the database")
 
 args = parser.parse_args()
 img_folder = args.img_folder
 label_map_csv = args.label_map_csv
-output = args.output
+output = args.output_path
 
 
 ## HELPER FUNCTIONS
